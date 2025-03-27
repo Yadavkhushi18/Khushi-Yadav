30a09db2c5eb6efbaad7dcbd0b9f781297087406
Yadavkhushi18 <ky5666789@gmail.com>
2025-03-27 22:47:51 +0530

    Time: 11 ms (95.51%), Space: 56.7 MB (82.05%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        int freq = 0, n = nums.size();
+        int maxFreq = 0, reqNum = nums.get(0), cnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums.get(i) == reqNum) cnt++;
+            else cnt--;
+            if (cnt == 0) {
+                reqNum = nums.get(i);
+                cnt = 1;
+            }
+        }
+        for (int num : nums) {
+            if (num == reqNum) maxFreq++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums.get(i) == reqNum) freq++;
+            if (freq * 2 > (i + 1) && (maxFreq - freq) * 2 > (n - i - 1)) return i;
+        }
+        return -1;
+    }
+}
